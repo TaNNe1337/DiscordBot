6b739145c66a12059a923c0f1e3123637dd2d731
TaNNe <tanne@hcs-cloud.ga>
2019-01-10 22:27:09 +0100

    initial commit

---
+++ ServerChanger/ServerChanger/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+using Discord;
+using Discord.WebSocket;
+using Discord.Commands;
+
+namespace ServerChanger
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
