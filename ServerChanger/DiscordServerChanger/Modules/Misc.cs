8c65fdd25c1d8b63ab1d228a599d6a0cd31bbb52
TaNNe <tanne@hcs-cloud.ga>
2019-01-16 20:45:05 +0100

    added command echo and userinfo

---
+++ ServerChanger/DiscordServerChanger/Modules/Misc.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Threading.Tasks;
+using Discord;
+using Discord.Commands;
+
+public class Misc : ModuleBase<SocketCommandContext>
+{
+	[Command("echo")]
+    public async Task Echo([Remainder]string message)
+    {
+        var embed = new EmbedBuilder();
+        embed.WithTitle("Echoed Message");
+        embed.WithDescription(message);
+        embed.WithColor(new Color(0, 255, 0));
+
+        await Context.Channel.SendMessageAsync("", false, embed.Build());
+    }
+    [Command("userinfo")]
+    public async Task Userinfo()
+    {
+        var embed = new EmbedBuilder();
+        embed.WithTitle("Echoed Message");
+        embed.WithDescription(Context.User.ToString());
+        embed.WithColor(new Color(0, 255, 0));
+
+        await ReplyAsync("", false, embed.Build());
+    }
+}
